2c3025e4382aac5891175ce90d24fd95200ad055
mohamdrajab5-svg <mohamdrajab5@gmail.com>
2025-10-30 01:16:24 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDRSweuWRvMaPj2KmKb-QDeEcHy1fxZPQA",
   authDomain: "studenthub-49c59.firebaseapp.com",
@@ -16,18 +9,6 @@ const firebaseConfig = {
   measurementId: "G-071WBYQF05"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firebaseConfig = {
-  apiKey: "AIzaSy...YOUR_KEY...xI",
-  authDomain: "student-hub.firebaseapp.com",
-  projectId: "student-hub",
-  storageBucket: "student-hub.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:..."
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+// Initialize Firebase (using the v8 syntax our scripts expect)
+firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
